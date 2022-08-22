a3ea86f56520c72c8f565ff169ba7a4a2957d031
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-22 15:56:45 +0530

    Update memoizatioonRecursiveF.java

--- Fibonacci Sequence/memoizatioonRecursiveF.java
+++ Fibonacci Sequence/memoizatioonRecursiveF.java
@@ -25,3 +25,33 @@ public class fibNumbers {
 
     }
 }
+
+
+// 2 nd Version
+
+
+public class fibNumbers {
+
+    long[] fibArray = new long[57];
+
+    private long fibonacci(int n) {
+        long fibValue;  //default value is 0
+        if (n <= 2) return 1;
+        else if (fibArray[n] != 0) return fibArray[n];
+        fibValue = fibonacci(n - 1) + fibonacci(n - 2);
+        fibArray[n] = fibValue;
+        return fibValue;
+    }
+
+
+
+    public static void main(String[] args){
+        fibNumbers ob = new fibNumbers();
+        System.out.println(ob.fibonacci(6));
+        System.out.println(ob.fibonacci(7));
+        System.out.println(ob.fibonacci(8));
+        System.out.println(ob.fibonacci(50));
+        System.out.println(ob.fibonacci(51));
+
+    }
+}
