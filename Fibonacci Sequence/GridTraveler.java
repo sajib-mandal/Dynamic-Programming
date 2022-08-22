0f79e45f5c75435f298cbf71860e6692b001b03d
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-22 19:16:54 +0530

    Update GridTraveler.java

--- Fibonacci Sequence/GridTraveler.java
+++ Fibonacci Sequence/GridTraveler.java
@@ -43,3 +43,30 @@ public class GridTraveler {
     }
 }
 
+
+// next memoization version
+
+import java.util.HashMap;
+
+public class GridTraveler {
+    public static Long gridTraveler(long m, long n, HashMap<String, Long> memo) {
+        String key = m + "," + n;
+        if (memo.containsKey(key))
+            return memo.get(key);
+
+        if (m == 1 || n == 1) return (long) 1;
+        if (m == 0 || n == 0) return (long) 0;
+        
+        memo.put(key, gridTraveler(m -1, n, memo) + gridTraveler(m, n - 1, memo));
+        return memo.get(key);
+    }
+
+    public static void main(String[] args) {
+        HashMap<String, Long> memo = new HashMap<>();
+        System.out.println(gridTraveler(1, 1, memo));
+        System.out.println(gridTraveler(2, 3, memo));
+        System.out.println(gridTraveler(3, 2, memo));
+        System.out.println(gridTraveler(3, 3, memo));
+        System.out.println(gridTraveler(18, 18, memo));
+    }
+}
