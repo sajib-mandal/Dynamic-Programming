fbd5112b7dfe2ff6307a4074aca50849e2297fad
Sajib Mandal <107785806+sajib-mandal@users.noreply.github.com>
2022-08-24 15:19:53 +0530

    Update canSum.java

--- Memoization & Recursive/canSum.java
+++ Memoization & Recursive/canSum.java
@@ -35,3 +35,46 @@ public class CanSum {
 
     }
 }
+
+
+//I dont know  its slow more work
+
+import java.util.*;
+
+public class CanSum {
+
+    public static Boolean canSum(int targetSum, int[] numbers, HashMap<Integer, Integer> memo) {
+        if (memo.containsKey(targetSum)) {
+            memo.get(targetSum);
+        }
+
+        if (targetSum == 0) return true;
+        if (targetSum < 0) return false;
+
+        for (int i = 0; i < numbers.length; i++) {
+            int reminder = targetSum - numbers[i];
+            if (canSum(reminder, numbers, memo) == true) {
+                memo.put(targetSum, reminder);
+                return true;
+            }
+        }
+        memo.get(targetSum);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        int[] numbers1 = new int[] {2, 3};
+        System.out.println(canSum(7, numbers1, memo));
+        int[] numbers2 = new int[]{5, 3, 4, 7};
+        System.out.println(canSum(7, numbers2, memo));
+        int[] numbers3 = new int[]{2, 4};
+        System.out.println(canSum(7, numbers3, memo));
+        int[] numbers4 = new int[]{2, 3, 5};
+        System.out.println(canSum(8, numbers4, memo));
+        int[] numbers5 = new int[]{7, 14};
+        System.out.println(canSum(300, numbers5, memo));
+    }
+}
+
+
